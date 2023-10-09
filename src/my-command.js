98db551ea124054f681ffba2450759a26b2ba247
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2023-10-09 15:04:53 +0400

    Support SVGs with non-zero viewBox origin

--- src/my-command.js
+++ src/my-command.js
@@ -1,5 +1,6 @@
 import path from "path";
 import sketch from "sketch";
+import util from "util";
 import dialog from "@skpm/dialog";
 
 export default function() {
@@ -25,6 +26,11 @@ export default function() {
           importer.prepareToImportFromURL_error(NSURL.fileURLWithPath(filepath), null);
         }
 
+        const viewBox = viewBoxForImporter(importer);
+        if (viewBox.x != 0 || viewBox.y != 0) {
+          recursivelyRebaseSVGElementToZeroOrigin(importer.graph().element(), viewBox);
+        }
+
         const width = importer.graph().element().width();
 
         const frame = NSMakeRect(currentX, 0, width, importer.graph().element().height());
@@ -39,4 +45,33 @@ export default function() {
         currentX += width + 20;
       })
   );
+}
+
+function viewBoxForImporter(importer) {
+  const raw = importer.graph().element().xml()
+    .stringFromAttributeWithName("viewBox")
+    .split(' ').map(Number);
+  return { x: raw[0], y: raw[1], width: raw[2], height: raw[3] };
+}
+
+function recursivelyRebaseSVGElementToZeroOrigin(container, currentViewBox) {
+  const children = util.toArray(container.children().allObjects());
+  children.forEach(el => {
+    if (el.isKindOfClass(SVGGroupElement)) {
+      recursivelyRebaseSVGElementToZeroOrigin(el, currentViewBox);
+    }
+    else if (el.isKindOfClass(SVGCircleShape) || el.isKindOfClass(SVGEllipseShape)) {
+      el.setCx(el.cx() - currentViewBox.x);
+      el.setCy(el.cy() - currentViewBox.y);
+    }
+    else if (el.isKindOfClass(SVGText)) {
+      el.setX(el.x() - currentViewBox.x);
+      el.setY(el.y() - currentViewBox.y);
+    }
+    else if (el.isKindOfClass(SVGPathShape)) {
+      const rebase = NSAffineTransform.transform();
+      rebase.translateXBy_yBy(-currentViewBox.x, -currentViewBox.y);
+      el.path().transformUsingAffineTransform(rebase);
+    }
+  });
 }
